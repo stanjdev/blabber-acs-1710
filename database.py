b56a025c79a461aad07a1ed52bc97b22468f5f37
Stanley Jeong <jeongstanley@gmail.com>
2022-09-20 16:44:15 -0700

    added Dockerfile and docker-compose.yml

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ import os
 from pymongo import MongoClient
 
 # MONGO_URI is Config Var for Heroku
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/blabber')
+# host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/blabber')
+host = os.environ.get('MONGODB_URI', 'mongodb://mongodb:27017/blabber')
 client = MongoClient(host=host)
 db = client.get_default_database()
 
